e36311e4074daddb99f6d9fe3d331dc640977729
Jishan jamal <44254341+zeeshan7417@users.noreply.github.com>
2022-03-06 11:37:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = require('./server');
 watchdog.phoneNumbersWatchdog();
 
 server.listen(cfg.port, function(err) {
-  console.log(err, server.address());
+  console.log(err, server);
   console.log(
     `Starting sample-conversations-masked-numbers at http://localhost:${cfg.port}`
   );
